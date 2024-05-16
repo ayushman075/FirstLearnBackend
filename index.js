dabe60ad640823a72701caee9516ebdf7297273e
Ayushman Kumar Pandey <kumarayushmanpandey075@gmail.com>
2024-05-16 13:18:41 +0530

    Database Setup

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-const express = require('express');
+import dotenv from "dotenv";
+import express from 'express';
 const app = express();
+import connectDB from "./src/db/index.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+dotenv.config({
+  path:'.env'
+});
+
+app.use(cors({
+  origin:process.env.CORS_ORIGIN,
+  credentials:true
+}));
+
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended:true,
+  limit:"16kb"
+}));
+app.use(express.static("public"));
+app.use(cookieParser())
+
+
+const port = process.env.PORT||8000;
+connectDB().then((res)=>
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  })
+).catch(()=>{
+//error handling start
+console.log("Error connecting to database !!")
+//error handling end
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Welcome to Firstlearn, on this line you are taking to Firstlearn server !!');
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
